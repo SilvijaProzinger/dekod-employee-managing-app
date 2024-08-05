b2cdb858a78df3213b5c398a26eb598e3d5ca22b
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-08-05 21:35:43 +0200

    create header component and call it in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { EmployeesListComponent } from '../employees/employees-list/employees-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, EmployeesListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
