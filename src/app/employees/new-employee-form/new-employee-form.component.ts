650f9336d9b004e0dd1d931dbcef68a4858ccec0
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-08-06 15:20:52 +0200

    add new employee form component and link to it

---
+++ src/app/employees/new-employee-form/new-employee-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-employee-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-employee-form.component.html',
+  styleUrl: './new-employee-form.component.scss'
+})
+export class NewEmployeeFormComponent {
+
+}
