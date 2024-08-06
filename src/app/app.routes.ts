b79ed9c6fb0798beca8dee73833a336e912d23a4
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-08-06 15:05:22 +0200

    rearrange home component template file and add link to new employee page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NewEmployeeFormComponent } from './employees/new-employee-form/new-employee-form.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'addNewEmployee', component: NewEmployeeFormComponent },
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { EmployeesListComponent } from '../employees/employees-list/employees-list.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, EmployeesListComponent],
+  imports: [HeaderComponent, EmployeesListComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
