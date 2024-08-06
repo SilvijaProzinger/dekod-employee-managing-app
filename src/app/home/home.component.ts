f1763e1ae352f79f0ce8ceaa5a845cb1e962d6d5
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-08-06 18:05:51 +0200

    move header to app component to always display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, EmployeesListComponent, RouterLink],
+  imports: [EmployeesListComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
