dbd90c48836de27af97977c0490901e5bfc6c15b
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-08-06 17:36:11 +0200

    finish new employee form and extra buttons style into separate file

--- src/app/employees/new-employee-form/new-employee-form.component.ts
+++ src/app/employees/new-employee-form/new-employee-form.component.ts
@@ -1,12 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-new-employee-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-employee-form.component.html',
-  styleUrl: './new-employee-form.component.scss'
+  styleUrl: './new-employee-form.component.scss',
 })
 export class NewEmployeeFormComponent {
+  employeeForm = this.fb.group({ 
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    jobTitle: ['', [Validators.required]],
+    dateOfBirth: ['', [Validators.required, this.validateDateOfBirth]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  validateDateOfBirth(control: FormControl): { [key: string]: boolean } | null {
+    const dateOfBirth = new Date(control.value).getFullYear();
+    const today = new Date().getFullYear();
+
+    // check if the new employee is not younger than 18
+    if (today - dateOfBirth < 18) {
+      return { invalidDateOfBirth: true };
+    }
+    return null;
+  }
+
+  onSubmit(): void {
+    if (this.employeeForm.valid) {
+      const newEmployee = this.employeeForm.value;
+      console.log(newEmployee);
+      this.employeeForm.reset();
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
