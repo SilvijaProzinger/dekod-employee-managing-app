a8c9632050cc57d9c9d8871925eef402cf21d82b
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-08-06 21:56:46 +0200

    move header and make content render conditionally based on url

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute, RouterLink } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  currentRoute = '';
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter(
+          (event): event is NavigationEnd => event instanceof NavigationEnd
+        )
+      )
+      .subscribe((event) => {
+        this.currentRoute = event.urlAfterRedirects;
+        console.log(this.currentRoute);
+      });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { EmployeesListComponent } from '../employees/employees-list/employees-list.component';
 import { RouterLink } from '@angular/router';
+import { SearchEmployeesComponent } from '../employees/search-employees/search-employees.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [EmployeesListComponent, RouterLink],
+  imports: [RouterLink, EmployeesListComponent, SearchEmployeesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
