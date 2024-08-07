127ca5a3262860bae12ceb3a94dfda137fe5db90
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-08-07 22:22:15 +0200

    add filter employees component

---
+++ src/app/filter-employees/filter-employees.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Employee } from '../types/types';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-filter-employees',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './filter-employees.component.html',
+  styleUrl: './filter-employees.component.scss',
+})
+export class FilterEmployeesComponent {
+  @Input() employees$: Observable<Employee[]> = new Observable<Employee[]>();
+  @Output() positionsOutput: EventEmitter<Set<string>> = new EventEmitter<
+    Set<string>
+  >();
+
+  positionsCheckboxes: string[] = [];
+  checkedPositions: Set<string> = new Set();
+
+  ngOnInit(): void {
+    this.employees$
+      .pipe(
+        map((employees: Employee[]) => {
+          const allPositions = employees.map((employee) => employee.jobTitle);
+          const positionsFiltered = allPositions.filter(
+            (title, index) => allPositions.indexOf(title) === index
+          );
+          this.positionsCheckboxes = positionsFiltered
+        })
+      )
+      .subscribe();
+  }
+
+  handleFilterChange(event: Event, position: string): void {
+    const checked = event.target as HTMLInputElement;
+    if (checked) {
+      this.checkedPositions.add(position);
+    } else {
+      this.checkedPositions.delete(position);
+    }
+    this.positionsOutput.emit(this.checkedPositions);
+  }
+}
