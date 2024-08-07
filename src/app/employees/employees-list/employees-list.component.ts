8f020a3ac89e460afc6a284c40a02d07497c0ff8
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-08-07 17:27:03 +0200

    add search to employees list component

--- src/app/employees/employees-list/employees-list.component.ts
+++ src/app/employees/employees-list/employees-list.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { Employee } from '../../types/types';
 import { EmployeeService } from '../shared/employees.service';
 import { CommonModule } from '@angular/common';
-import { Observable, of } from 'rxjs';
+import { Observable, of, map, BehaviorSubject } from 'rxjs';
 import { catchError, finalize, startWith } from 'rxjs/operators';
+import { SearchEmployeesComponent } from '../search-employees/search-employees.component';
 
 @Component({
   selector: 'app-employees-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchEmployeesComponent],
   templateUrl: './employees-list.component.html',
   styleUrl: './employees-list.component.scss',
 })
 export class EmployeesListComponent {
-  employees$: Observable<Employee[]> = of([]);
+  // initialize employees observer as behavior subject to cache http get response as api data changes randomly
+  employees$ = new BehaviorSubject<Employee[]>([]);
+  filteredEmployees$: Observable<Employee[]> = this.employees$.asObservable();
   isLoading = false;
   fetchEmployeesError: string | null = null;
 
@@ -25,17 +28,32 @@ export class EmployeesListComponent {
 
   initEmployees(): void {
     this.isLoading = true;
-    this.employees$ = this.employeeService.getEmployees().pipe(
-      startWith([]),
-      catchError((error: Error) => {
-        console.log(error);
-        this.fetchEmployeesError =
-          'There has been an error while getting the employees data. Please try again.';
-        return of([]);
-      }),
-      finalize(() => {
-        this.isLoading = false;
-      })
+    this.employeeService
+      .getEmployees()
+      .pipe(
+        startWith([]),
+        catchError((error: Error) => {
+          console.log(error);
+          this.fetchEmployeesError =
+            'There has been an error while getting the employees data. Please try again.';
+          return of([]);
+        }),
+        finalize(() => {
+          this.isLoading = false;
+        })
+      )
+      .subscribe((employees: Employee[]) => this.employees$.next(employees));
+  }
+
+  onSearchChange(search: string): void {
+    this.filteredEmployees$ = this.employees$.pipe(
+      map((employees: Employee[]) =>
+        employees.filter(
+          (employee: Employee) =>
+            employee.firstName.toLowerCase().includes(search.toLowerCase()) ||
+            employee.lastName.toLowerCase().includes(search.toLowerCase())
+        )
+      )
     );
   }
 }
