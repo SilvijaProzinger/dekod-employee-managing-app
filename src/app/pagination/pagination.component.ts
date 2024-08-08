325552cd1664e2835a78a4da689f2818699a90e4
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-08-08 16:05:55 +0200

    create pagination component and changePage function

---
+++ src/app/pagination/pagination.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.scss',
+})
+export class PaginationComponent {
+  @Input() totalItems: number = 0;
+  @Input() pageSize: number = 9;
+  @Input() currentPage: number = 1;
+
+  @Output() pageChange = new EventEmitter<number>();
+
+  get totalPages(): number {
+    return Math.floor(this.totalItems / this.pageSize);
+  }
+
+  changePage(page: number): void {
+    if (page <= this.totalPages) {
+      this.pageChange.emit(page);
+    }
+  }
+}
